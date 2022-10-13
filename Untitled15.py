d0c34988389a6a006f3c529f4320a75d3376e376
Zagi71 <115731816+Zagi71@users.noreply.github.com>
2022-10-13 10:36:36 -0700

    Add files via upload
    Sequence cleanup
    In this exercise, you will create a program to “clean up” a sequence of DNA by removing ambiguous bases (denoted by “N”) output from a sequencer.
    Your task is to create a Python program called seqCleaner that
    asks for and collects a sequence of DNA using input()
    removes the ambiguous parts of the sequence, outputs the “cleaned” sequence, replacing the ambiguous parts with a count in {}’s. For example, if I
    enter the sequence of DNA “AaNNNNNNGTC” (without quotes), the program will output:
    AA{6}GTC

---
+++ Untitled15.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#!/usr/bin/env python3 
+# Name: Zarghaam Abbas 
+# Group Members: Anand Verna , Andrew Matsiev, Rogelio Chavez, Samuel Hirsch
+
+'''
+Read a DNA string from user input and return a collapsed substring of embedded Ns to: {count}.
+
+Example: 
+ input: AaNNNNNNGTC
+output: AA{6}GTC
+
+Any lower case letters are converted to uppercase
+'''
+
+class DNAstring (str):
+    def length (self):
+        return (length(self))
+    
+    def purify(self):
+        ''' Return an upcased version of the string, collapsing a single run of Ns.
+            There is at most one continous Block of Ns in DNA sequence.
+            Therefore, if the number of the Ns is not zero, then all the Ns are consective.
+        '''
+        #This line of code willconvert the DNA sequence to UPPERCASE.
+        self=self.upper()
+        #This will create a variable count that store the Number of times N occur
+        cont=self.count('N')
+        #If there is a Block of Ns we will Replace it with {Length of Ns}, The code in if block just Do that
+        if cont!=0:
+            #We will find the starting adress of Ns, Block,
+            start=self.index('N')
+            #This is a temporary string that Store Number of Ns inside Curly Brackets i.e {5}
+            replace='{'+str(cont)+'}'
+            #This Line of code will Replace the Ns with empty space mean it just delete all Ns.
+            self=self.replace('N','')
+            #This code Add the replace string where the block of Ns was present.
+            self=self[0:start]+replace+self[start:len(self)]
+        #return Upper() cased string with collapsing of Ns if any,
+        return self
+
+    
+def main():
+    ''' Get user DNA data and clean it up.'''
+    data = input('DNA data?')
+    thisDNA = DNAstring (data)
+    pureData = thisDNA.purify()
+    print (pureData)
+    
+main()
+
+
+# In[ ]:
+
+
+
+
